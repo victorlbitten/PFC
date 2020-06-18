599e9abbc1cd24f3ee2bc7446b6cfc22c9792d22
victorlbitten <victorlbitten@gmail.com>
2020-06-18 14:38:05 -0300

    Novos estilos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/components/Header.css';
+import logo from '../settings.svg';
 
 
 const Header = () => (
-  <header>
-    <h1>Base React App</h1>
-    <NavLink to="/"
-      activeClassName="is-active">
-        Home
-    </NavLink>
-
-    <NavLink to="/about">
-      About
-    </NavLink>
+  <header className="header">
+    <div className="header--container">
+      <span className="header--title">
+        <img src={logo} className="header--logo" alt="logo"></img>
+        NextAPI
+      </span>
+      <div className="menu">
+          <NavLink exact to="/"
+            className="menu--element"
+            activeClassName="menu--element--active">
+              Home
+          </NavLink>
+          <NavLink to="/about"
+            className="menu--element"
+            activeClassName="menu--element--active">
+            About
+          </NavLink>
+      </div>
+    </div>
   </header>
 )
 
--- src/components/routers/Routes.js
+++ src/components/routers/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from '../App';
 import Header from '../Header';
