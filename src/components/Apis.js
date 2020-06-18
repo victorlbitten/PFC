2913c5c699c9a3921cc19b6ed78c474e2c9aed6f
victorlbitten <victorlbitten@gmail.com>
2020-06-18 14:51:15 -0300

    App -> Apis

---
+++ src/components/Apis.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/components/Apis.css';
+
+export default class Apis extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {apis: []};
+  }
+
+  render() {
+    return <div>APIS</div>
+  }
+}
