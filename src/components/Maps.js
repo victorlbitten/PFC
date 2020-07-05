278b7fd8f055f49a52d8741badba36e38340728f
victorlbitten <victorlbitten@gmail.com>
2020-07-05 09:38:21 -0300

    Mapas parecem funcionar corretamente

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MapElement from './MapElement';
 import '../styles/components/Maps.css';
 
 export default class Maps extends React.Component {
@@ -9,21 +10,26 @@ export default class Maps extends React.Component {
     }
 
     this.handleChanges = this.handleChanges.bind(this);
+    this.show = this.show.bind(this);
   }
 
   handleChanges(element, elementCurrentName) {
     this.setState((state) => {
       const elementParent = getTargetElementParent(state, element);
-      promoteElementChanges(state, element, elementCurrentName, elementParent);
+      promoteElementChanges(element, elementCurrentName, elementParent);
       return {maps: state.maps};
     })
   }
 
-  componentDidMount () {
+  componentWillMount () {
     this.setState((state) => {
       appendAddButonToContainers(state.maps);
       return {maps: state.maps}
     })
+  }
+
+  show() {
+    console.log(this.state.maps);
   }
 
   render() {
@@ -33,28 +39,66 @@ export default class Maps extends React.Component {
           mapping={this.state.maps}
           addOrEdit={this.handleChanges}  
         />
+        <button onClick={this.show}>show</button>
       </div>
     )
   }
 }
 
 function DrawMaps ({mapping, addOrEdit}) {
-  const initialDepth = 0;
-  return (
-    <div className="maps-container">
-      {Object.entries(mapping).map(([name, values], index) => {
+  const drawMapElements = (element, depth=0, path=null) => {
+    const elementsToDraw = Object.entries(element);
+    return elementsToDraw.map((element, index) => {
+      const getElementProperties = (element, depth) => {
+        // Definição de constantes e métodos
+        const elementType = element[1].type;
+        const containerizedTypes = ['object', 'object_array'];
+        const isContainerizedElement = containerizedTypes.includes(elementType);
+        const elementKey = `${index}${depth}${Math.random()}`;
+
+        return [isContainerizedElement, elementKey];
+      }
+
+      const setElementPath = (path) => {
+        element[1].relativePath = (path)
+          ? [...path, element[0]]
+          : [element[0]];
+      }
+
+      const [isContainerizedElement, elementKey] = getElementProperties(element, depth);
+
+      const containerizedElement = (elementKey, element) => {
         return (
-          <MapElement key={`${index}${initialDepth}`}
-            addOrEdit={addOrEdit}
-            elementName={name}
-            elementInfo={values}
-            depth={initialDepth}
+          <div className="containerized-type" key={elementKey}>
+            <MapElement
+              element={element}
+              depth={depth}
+              saveChanges={addOrEdit}
+            />
+            {drawMapElements(element[1].mapping, depth + 1, element[1].relativePath)}
+          </div>
+        )
+      }
+
+      const nonContainerizedElement = (elementKey, element) => {
+        return (
+          <MapElement key={elementKey} 
+            element={element}
+            depth={depth}
+            saveChanges={addOrEdit}
           />
         )
-      })
-      .sort((a) => sortAddBtnAsLast(a.props.elementInfo))}
-    </div>
-  )
+      }
+
+      // Chamadas a métodos e função em si
+      setElementPath(path);
+
+      return (isContainerizedElement)
+        ? containerizedElement(elementKey, element)
+        : nonContainerizedElement(elementKey, element);
+    })
+  }
+  return drawMapElements(mapping);
 }
 
 function sortAddBtnAsLast (first) {
@@ -63,56 +107,6 @@ function sortAddBtnAsLast (first) {
     : 0
 
 }
-
-function MapElement ({addOrEdit, elementName, elementInfo, depth, path=null}) {
-  elementInfo.relativePath = (path)
-    ? [...path, elementName]
-    : [elementName];
-  const shouldDrawSubmaps = (elementInfo.type !== 'property');
-  const subMaps = (shouldDrawSubmaps)
-    ? Object.entries(elementInfo.mapping)
-    : null;
-  
-  return (
-    <div className="map-elements-container">
-      <div className="map-element"
-        style={{'marginLeft': (20*depth + 'px')}}
-        onClick={() => addOrEdit(elementInfo, elementName)}>
-          {(elementInfo.type === 'add')
-            ? '+'
-            : elementName
-          }
-      </div>
-
-      {(shouldDrawSubmaps)
-        ? <DrawSubmaps
-          addOrEdit={addOrEdit}
-          subMaps={subMaps}
-          depth={depth}
-          path={elementInfo.relativePath} 
-        />
-        : null
-      }
-    </div>
-  )
-}
-
-function DrawSubmaps ({addOrEdit, subMaps, depth, path}) {
-  return(
-    subMaps.map(([name, values], index) => (
-        <MapElement
-        key={`${index}${depth}`}
-        addOrEdit={addOrEdit}
-        elementName={name}
-        elementInfo={values}
-        depth={depth+1}
-        path={path}
-      />
-    ))
-    .sort((a) => sortAddBtnAsLast(a.props.elementInfo))
-  )
-}
-
 
 const getTargetElementParent = (state, element) => {
   const getNextPath = (currentPath) => {
@@ -141,24 +135,26 @@ const getTargetElementParent = (state, element) => {
   return iterateTillElementParent(state.maps, relativePathFirstStep, numberOfIterations);
 }
 
-const promoteElementChanges = (state, element, elementCurrentName, elementParent) => {
-  // zueira -> Remover replacer e parâmetro state
-  const replacer = Object.assign({}, state.maps.sensor.mapping.name);
-  elementCurrentName = 'NewName';
-
+const promoteElementChanges = (element, elementCurrentName, elementParent) => {
   const elementFormerName = element.relativePath[element.relativePath.length - 1];
   const elementNameHasChanged = (elementFormerName !== elementCurrentName);
 
   // Substituir os dados antigos pelos atuais
-  elementParent[elementCurrentName] = replacer;
+  elementParent[elementCurrentName] = element;
   
-  if (element.type === 'add') {
-    appendNewElement(elementParent, true);
-    return;
-  }
-
   if (elementNameHasChanged) {
     delete elementParent[elementFormerName];
+  }
+
+  if (element.isNewElement) {
+    appendNewElement(elementParent, true);
+    delete element.isNewElement;
+  }
+
+  if(['object', 'object_array'].includes(element.type)) {
+    if(!Object.keys(element.mapping).length) {
+      appendNewElement(element, false);
+    }
   }
 }
 
